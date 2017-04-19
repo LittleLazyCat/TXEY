e9045d2b7ad6cf9a903e49f99d4be103a462a8b8
LittleLazyCat <850162412@qq.com>
2017-04-19 14:07:47 +0800

    实现获得员工信息方法

---
+++ RecordSystem/src/main/java/com/txey/record/dao/mybatis/EmployeeMybatisTemplateDao.java
@@ -0,0 +1,19 @@
+package com.txey.record.dao.mybatis;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.txey.record.model.Employee;
+
+@Repository
+public class EmployeeMybatisTemplateDao {
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	public List<Employee> getEmployee(){
+		EmployeeMybatisDao employeeMybatisDao = sqlSessionTemplate.getMapper(EmployeeMybatisDao.class);
+		return employeeMybatisDao.getEmployee();
+	}
+}
